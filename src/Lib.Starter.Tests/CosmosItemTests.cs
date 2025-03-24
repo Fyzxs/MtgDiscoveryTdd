323124bf7722aa5908ecec4a74512b60af51e99d
Quinn Gil <Fyzxs@users.noreply.github.com>
2025-03-23 20:32:43 -0700

    . r create class

--- src/Lib.Starter.Tests/CosmosItemTests.cs
+++ src/Lib.Starter.Tests/CosmosItemTests.cs
@@ -3,8 +3,19 @@
 [TestClass]
 public sealed class CosmosItemTests
 {
-    [TestMethod]
-    public void TestMethod1()
+    [TestMethod, TestCategory("unit")]
+    public void CosmosItem_Should_Exist()
     {
+        //arrange
+
+        //act
+        new CosmosItem();
+
+        //assert
+
     }
+}
+
+public /* cosmos requires */ class CosmosItem
+{
 }
