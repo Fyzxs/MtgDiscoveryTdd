9eaed399051e780864f700824b8566bd69a92a09
Quinn Gil <Fyzxs@users.noreply.github.com>
2025-03-23 23:25:19 -0700

    .r created class

---
+++ src/Lib.Cosmos.Tests/CosmosItemTests.cs
@@ -0,0 +1,20 @@
+﻿namespace Lib.Cosmos.Tests;
+
+[TestClass]
+public class CosmosItemTests
+{
+    [TestMethod, TestCategory("unit")]
+    public void CosmosItem_ShouldExist()
+    {
+        //arrange
+
+        //act
+        CosmosItem _ = new CosmosItem();
+
+        //assert
+    }
+}
+
+public class CosmosItem
+{
+}
