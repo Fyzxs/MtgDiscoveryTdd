d46575682af4d39e4e0797e33a9d916536006b0e
Quinn Gil <Fyzxs@users.noreply.github.com>
2025-03-25 00:05:55 -0700

    . R remove CA2225"

--- src/Lib.Universal/Primitives/ToSystemType.cs
+++ src/Lib.Universal/Primitives/ToSystemType.cs
@@ -7,9 +7,4 @@ public abstract class ToSystemType<TSystemType>
     public static implicit operator TSystemType([NotNull] ToSystemType<TSystemType> source) => source.AsSystemType();
 
     public abstract TSystemType AsSystemType();
-
-    public TSystemType ToTSystemType()
-    {
-        throw new System.NotImplementedException();
-    }
 }
