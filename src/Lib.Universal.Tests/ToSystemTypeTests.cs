ad0445bb368b57bf600d75f452d1bd538255be11
Quinn Gil <Fyzxs@users.noreply.github.com>
2025-03-24 22:08:27 -0700

    . r add class

---
+++ src/Lib.Universal.Tests/ToSystemTypeTests.cs
@@ -0,0 +1,21 @@
+﻿namespace Lib.Universal.Tests;
+
+[TestClass]
+public class ToSystemTypeTests
+{
+    [TestMethod, TestCategory("unit")]
+    public void ToSystemType_ShouldExist()
+    {
+        //arrange
+
+        //act
+        _ = new ToSystemType();
+
+        //assert
+
+    }
+}
+
+public class ToSystemType
+{
+}
