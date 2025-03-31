f4db10e393f8b1251710133221042c8057ace02c
Quinn Gil <Fyzxs@users.noreply.github.com>
2025-03-31 00:04:34 -0700

    . t create ToSystemType marker class test

--- src/Lib.Cosmos.Tests/Primitives/CosmosAccountNameTests.cs
+++ src/Lib.Cosmos.Tests/Primitives/CosmosAccountNameTests.cs
@@ -5,7 +5,7 @@ using Lib.Universal.Primitives;
 namespace Lib.Cosmos.Tests.Primitives;
 
 [TestClass]
-public sealed class CosmosAccountNameTests
+public sealed class CosmosAccountNameTests : BaseToSystemTypeTests<CosmosAccountName, string>
 {
     [TestMethod, TestCategory("unit")]
     public void CosmosAccountName_ShouldBeAbstract()
@@ -28,6 +28,36 @@ public sealed class CosmosAccountNameTests
 
         //act
         bool actual = subject.IsSubclassOf(typeof(ToSystemType<string>));
+
+        //assert
+        _ = actual.Should().BeTrue();
+    }
+}
+
+[TestClass]
+public abstract class BaseToSystemTypeTests<TType, TSystemType>
+{
+    [TestMethod, TestCategory("unit")]
+    public void Class_ShouldBeAbstract()
+    {
+        //arrange
+        Type subject = typeof(TType);
+
+        //act
+        bool actual = subject.IsAbstract;
+
+        //assert
+        _ = actual.Should().BeTrue();
+    }
+
+    [TestMethod, TestCategory("unit")]
+    public void ClassShouldDeriveFromToSystemType()
+    {
+        //arrange
+        Type subject = typeof(TType);
+
+        //act
+        bool actual = subject.IsSubclassOf(typeof(ToSystemType<TSystemType>));
 
         //assert
         _ = actual.Should().BeTrue();
