a886bdc36482eb6da5206dd75747a685361859ad
Quinn Gil <Fyzxs@users.noreply.github.com>
2025-03-31 00:13:37 -0700

    . t block scope

--- src/Lib.Cosmos.Tests/Primitives/CosmosAccountNameTests.cs
+++ src/Lib.Cosmos.Tests/Primitives/CosmosAccountNameTests.cs
@@ -2,12 +2,13 @@
 using Lib.Universal.Primitives;
 using TestConvenience.Universal.Tests;
 
-namespace Lib.Cosmos.Tests.Primitives;
+namespace Lib.Cosmos.Tests.Primitives
+{
+    [TestClass]
+    public sealed class CosmosAccountNameTests : BaseToSystemTypeTests<CosmosAccountName, string>;
 
-[TestClass]
-public sealed class CosmosAccountNameTests : BaseToSystemTypeTests<CosmosAccountName, string>;
+    [TestClass]
+    public sealed class CosmosDatabaseNameTests : BaseToSystemTypeTests<CosmosDatabaseName, string>;
 
-[TestClass]
-public sealed class CosmosDatabaseNameTests : BaseToSystemTypeTests<CosmosDatabaseName, string>;
-
-public abstract class CosmosDatabaseName : ToSystemType<string>;
+    public abstract class CosmosDatabaseName : ToSystemType<string>;
+}
