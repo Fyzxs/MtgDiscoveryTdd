89bbd6e6f6492b349983ad0b96e1793d2ef823b9
Quinn Gil <Fyzxs@users.noreply.github.com>
2025-03-31 00:09:03 -0700

    . t remove duplicates

--- src/Lib.Cosmos.Tests/Primitives/CosmosAccountNameTests.cs
+++ src/Lib.Cosmos.Tests/Primitives/CosmosAccountNameTests.cs
@@ -1,36 +1,7 @@
-﻿using System;
-using Lib.Cosmos.Primitives;
-using Lib.Universal.Primitives;
+﻿using Lib.Cosmos.Primitives;
 using TestConvenience.Universal.Tests;
 
 namespace Lib.Cosmos.Tests.Primitives;
 
 [TestClass]
-public sealed class CosmosAccountNameTests : BaseToSystemTypeTests<CosmosAccountName, string>
-{
-    [TestMethod, TestCategory("unit")]
-    public void CosmosAccountName_ShouldBeAbstract()
-    {
-        //arrange
-        Type subject = typeof(CosmosAccountName);
-
-        //act
-        bool actual = subject.IsAbstract;
-
-        //assert
-        _ = actual.Should().BeTrue();
-    }
-
-    [TestMethod, TestCategory("unit")]
-    public void CosmosAccountName_ShouldDeriveFromToSystemType()
-    {
-        //arrange
-        Type subject = typeof(CosmosAccountName);
-
-        //act
-        bool actual = subject.IsSubclassOf(typeof(ToSystemType<string>));
-
-        //assert
-        _ = actual.Should().BeTrue();
-    }
-}
+public sealed class CosmosAccountNameTests : BaseToSystemTypeTests<CosmosAccountName, string>;
