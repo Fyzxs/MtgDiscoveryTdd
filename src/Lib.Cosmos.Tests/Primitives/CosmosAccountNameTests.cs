9abcd19c02a12bf7f26ba7280df9f70308940340
Quinn Gil <Fyzxs@users.noreply.github.com>
2025-03-31 00:11:00 -0700

    ^ F create CosmosDatabaseName

--- src/Lib.Cosmos.Tests/Primitives/CosmosAccountNameTests.cs
+++ src/Lib.Cosmos.Tests/Primitives/CosmosAccountNameTests.cs
@@ -1,7 +1,13 @@
 ﻿using Lib.Cosmos.Primitives;
+using Lib.Universal.Primitives;
 using TestConvenience.Universal.Tests;
 
 namespace Lib.Cosmos.Tests.Primitives;
 
 [TestClass]
 public sealed class CosmosAccountNameTests : BaseToSystemTypeTests<CosmosAccountName, string>;
+
+[TestClass]
+public sealed class CosmosDatabaseNameTests : BaseToSystemTypeTests<CosmosDatabaseName, string>;
+
+public abstract class CosmosDatabaseName : ToSystemType<string>;
