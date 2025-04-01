1b7e38d86597bc9f384cdca60fdc11180ec7ada1
Quinn Gil <Fyzxs@users.noreply.github.com>
2025-03-31 23:27:58 -0700

    . t fake

---
+++ src/Lib.Cosmos.Tests/Fakes/ItemResponseFake.cs
@@ -0,0 +1,13 @@
+﻿using System.Net;
+using Microsoft.Azure.Cosmos;
+
+namespace Lib.Cosmos.Tests.Fakes;
+
+public sealed class ItemResponseFake : ItemResponse<object>
+{
+    public object ResourceResult { get; init; }
+    public HttpStatusCode StatusCodeResult { get; init; }
+
+    public override object Resource => ResourceResult;
+    public override HttpStatusCode StatusCode => StatusCodeResult;
+}
