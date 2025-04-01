697fedf2190f0af4ac1bf0d77be83c5655206c29
Quinn Gil <Fyzxs@users.noreply.github.com>
2025-03-31 21:40:57 -0700

    . r file scope

--- src/Lib.Cosmos.Tests/Primitives/CosmosDatabaseNameTests.cs
+++ src/Lib.Cosmos.Tests/Primitives/CosmosDatabaseNameTests.cs
@@ -1,12 +1,10 @@
 ﻿using Lib.Cosmos.Primitives;
 using TestConvenience.Universal.Tests;
 
-namespace Lib.Cosmos.Tests.Primitives
-{
+namespace Lib.Cosmos.Tests.Primitives;
 
-    [TestClass]
-    public sealed class CosmosDatabaseNameTests : BaseToSystemTypeTests<CosmosDatabaseName, string>;
+[TestClass]
+public sealed class CosmosDatabaseNameTests : BaseToSystemTypeTests<CosmosDatabaseName, string>;
 
-    [TestClass]
-    public sealed class CosmosCollectionNameTests : BaseToSystemTypeTests<CosmosCollectionName, string>;
-}
+[TestClass]
+public sealed class CosmosCollectionNameTests : BaseToSystemTypeTests<CosmosCollectionName, string>;
