34031127b8b28c13b086a21b35024f776f2e59e7
Quinn Gil <Fyzxs@users.noreply.github.com>
2025-03-31 22:35:24 -0700

    . a formatting

--- src/Lib.Cosmos/Apis/CosmosItemResponse.cs
+++ src/Lib.Cosmos/Apis/CosmosItemResponse.cs
@@ -6,6 +6,7 @@ namespace Lib.Cosmos.Apis;
 public abstract class CosmosItemResponse<T> : ToSystemType<T>
 {
     public abstract T Value { get; }
+
     public abstract HttpStatusCode StatusCode { get; }
 
     public bool IsSuccessfulStatusCode() => 200 <= (int)StatusCode && (int)StatusCode <= 299;
