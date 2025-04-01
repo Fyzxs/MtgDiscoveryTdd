e384e17bfa5152d9bbe0eab7755a0e40f1333d93
Quinn Gil <Fyzxs@users.noreply.github.com>
2025-03-31 22:49:49 -0700

    . a formatting

--- src/Lib.Cosmos.Tests/CosmosItemTests.cs
+++ src/Lib.Cosmos.Tests/CosmosItemTests.cs
@@ -2,171 +2,169 @@
 using System.Reflection;
 using Newtonsoft.Json;
 
-namespace Lib.Cosmos.Tests
+namespace Lib.Cosmos.Tests;
+
+[TestClass]
+public sealed class CosmosItemTests
 {
-
-    [TestClass]
-    public sealed class CosmosItemTests
+    [TestMethod, TestCategory("unit")]
+    public void CosmosItem_ShouldExist()
     {
-        [TestMethod, TestCategory("unit")]
-        public void CosmosItem_ShouldExist()
-        {
-            //arrange
+        //arrange
 
-            //act
-            CosmosItem _ = new();
+        //act
+        CosmosItem _ = new();
 
-            //assert
-        }
+        //assert
+    }
 
-        [TestMethod, TestCategory("unit")]
-        public void Id_ShouldExist()
-        {
-            //arrange
-            CosmosItem subject = new();
+    [TestMethod, TestCategory("unit")]
+    public void Id_ShouldExist()
+    {
+        //arrange
+        CosmosItem subject = new();
 
-            //act
-            string _ = subject.Id;
+        //act
+        string _ = subject.Id;
 
-            //assert
-        }
+        //assert
+    }
 
-        [TestMethod, TestCategory("unit")]
-        public void Id_ShouldSerializeLowerSnakeCase()
-        {
-            //arrange
-            CosmosItem subject = new() { Id = "SomeValue" };
+    [TestMethod, TestCategory("unit")]
+    public void Id_ShouldSerializeLowerSnakeCase()
+    {
+        //arrange
+        CosmosItem subject = new() { Id = "SomeValue" };
 
-            //act
-            string actual = JsonConvert.SerializeObject(subject);
+        //act
+        string actual = JsonConvert.SerializeObject(subject);
 
-            //assert
-            _ = actual.Should().Contain("\"id\":\"SomeValue\"");
-        }
+        //assert
+        _ = actual.Should().Contain("\"id\":\"SomeValue\"");
+    }
 
-        [TestMethod, TestCategory("unit")]
-        public void Partition_ShouldExist()
-        {
-            //arrange
+    [TestMethod, TestCategory("unit")]
+    public void Partition_ShouldExist()
+    {
+        //arrange
 
-            //act
-            string _ = new CosmosItem().Partition;
+        //act
+        string _ = new CosmosItem().Partition;
 
-            //assert
-        }
+        //assert
+    }
 
-        [TestMethod, TestCategory("unit")]
-        public void Partition_ShouldSerializeLowerSnakeCase()
-        {
-            //arrange
-            CosmosItem subject = new() { Partition = "myValue" };
+    [TestMethod, TestCategory("unit")]
+    public void Partition_ShouldSerializeLowerSnakeCase()
+    {
+        //arrange
+        CosmosItem subject = new() { Partition = "myValue" };
 
-            //act
-            string actual = JsonConvert.SerializeObject(subject);
+        //act
+        string actual = JsonConvert.SerializeObject(subject);
 
-            //assert
-            _ = actual.Should().Contain("\"partition\":\"myValue\"");
-        }
+        //assert
+        _ = actual.Should().Contain("\"partition\":\"myValue\"");
+    }
 
-        [TestMethod, TestCategory("unit")]
-        public void ItemType_ShouldExist()
-        {
-            //arrange
+    [TestMethod, TestCategory("unit")]
+    public void ItemType_ShouldExist()
+    {
+        //arrange
 
-            //act
-            string _ = new CosmosItem().ItemType;
+        //act
+        string _ = new CosmosItem().ItemType;
 
-            //assert
-        }
+        //assert
+    }
 
-        [TestMethod, TestCategory("unit")]
-        public void ItemType_ShouldSerializeLowerSnakeCase()
-        {
-            //arrange
-            CosmosItem subject = new() { ItemType = "myType" };
+    [TestMethod, TestCategory("unit")]
+    public void ItemType_ShouldSerializeLowerSnakeCase()
+    {
+        //arrange
+        CosmosItem subject = new() { ItemType = "myType" };
 
-            //act
-            string actual = JsonConvert.SerializeObject(subject);
+        //act
+        string actual = JsonConvert.SerializeObject(subject);
 
-            //assert
-            _ = actual.Should().Contain("\"item_type\":\"myType\"");
-        }
+        //assert
+        _ = actual.Should().Contain("\"item_type\":\"myType\"");
+    }
 
-        [TestMethod, TestCategory("unit")]
-        public void ItemType_ShouldReturnFqdn()
-        {
-            //arrange
-            CosmosItem subject = new();
+    [TestMethod, TestCategory("unit")]
+    public void ItemType_ShouldReturnFqdn()
+    {
+        //arrange
+        CosmosItem subject = new();
 
-            //act
-            string actual = subject.ItemType;
+        //act
+        string actual = subject.ItemType;
 
-            //assert
-            _ = actual.Should().Be("Lib.Cosmos.CosmosItem");
-        }
+        //assert
+        _ = actual.Should().Be("Lib.Cosmos.CosmosItem");
+    }
 
-        [TestMethod, TestCategory("unit")]
-        public void CreatedDate_ShouldExist()
-        {
-            //arrange
+    [TestMethod, TestCategory("unit")]
+    public void CreatedDate_ShouldExist()
+    {
+        //arrange
 
-            //act
-            string _ = new CosmosItem().CreatedDate;
+        //act
+        string _ = new CosmosItem().CreatedDate;
 
-            //assert
-        }
+        //assert
+    }
 
-        [TestMethod, TestCategory("unit")]
-        public void CreatedDate_ShouldSerializeLowerSnakeCase()
-        {
-            //arrange
-            CosmosItem subject = new() { CreatedDate = "myDate" };
+    [TestMethod, TestCategory("unit")]
+    public void CreatedDate_ShouldSerializeLowerSnakeCase()
+    {
+        //arrange
+        CosmosItem subject = new() { CreatedDate = "myDate" };
 
-            //act
-            string actual = JsonConvert.SerializeObject(subject);
+        //act
+        string actual = JsonConvert.SerializeObject(subject);
 
-            //assert
-            _ = actual.Should().Contain("\"created_date\":\"myDate\"");
-        }
+        //assert
+        _ = actual.Should().Contain("\"created_date\":\"myDate\"");
+    }
 
-        [TestMethod, TestCategory("unit")]
-        public void CreatedDate_ShouldReturnUtcNowIso8601()
-        {
-            //arrange
-            CosmosItem subject = new();
+    [TestMethod, TestCategory("unit")]
+    public void CreatedDate_ShouldReturnUtcNowIso8601()
+    {
+        //arrange
+        CosmosItem subject = new();
 
-            //act
-            string actual = subject.CreatedDate;
+        //act
+        string actual = subject.CreatedDate;
 
-            //assert
-            DateTime universalTime = DateTime.Parse(actual).ToUniversalTime();
-            _ = universalTime.Should().BeCloseTo(DateTime.UtcNow, TimeSpan.FromSeconds(5));
-        }
+        //assert
+        DateTime universalTime = DateTime.Parse(actual).ToUniversalTime();
+        _ = universalTime.Should().BeCloseTo(DateTime.UtcNow, TimeSpan.FromSeconds(5));
+    }
 
-        [TestMethod, TestCategory("unit")]
-        public void Id_ShouldBeOverridable()
-        {
-            //arrange
-            PropertyInfo propertyInfo = typeof(CosmosItem).GetProperty("Id");
+    [TestMethod, TestCategory("unit")]
+    public void Id_ShouldBeOverridable()
+    {
+        //arrange
+        PropertyInfo propertyInfo = typeof(CosmosItem).GetProperty("Id");
 
-            //act
-            bool actual = propertyInfo?.GetMethod is { IsVirtual: true, IsFinal: false };
+        //act
+        bool actual = propertyInfo?.GetMethod is { IsVirtual: true, IsFinal: false };
 
-            //assert
-            _ = actual.Should().BeTrue();
-        }
+        //assert
+        _ = actual.Should().BeTrue();
+    }
 
-        [TestMethod, TestCategory("unit")]
-        public void Partition_ShouldBeOverridable()
-        {
-            //arrange
-            PropertyInfo propertyInfo = typeof(CosmosItem).GetProperty("Partition");
+    [TestMethod, TestCategory("unit")]
+    public void Partition_ShouldBeOverridable()
+    {
+        //arrange
+        PropertyInfo propertyInfo = typeof(CosmosItem).GetProperty("Partition");
 
-            //act
-            bool actual = propertyInfo?.GetMethod is { IsVirtual: true, IsFinal: false };
+        //act
+        bool actual = propertyInfo?.GetMethod is { IsVirtual: true, IsFinal: false };
 
-            //assert
-            _ = actual.Should().BeTrue();
-        }
+        //assert
+        _ = actual.Should().BeTrue();
     }
 }
