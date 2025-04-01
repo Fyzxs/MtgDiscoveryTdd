a972797eaf355e37a7be8d11369f11044cee2bab
Quinn Gil <Fyzxs@users.noreply.github.com>
2025-03-31 21:44:32 -0700

    . f class exists

---
+++ src/Lib.Cosmos.Tests/Adapters/CosmosContainerUpsertAdapterTests.cs
@@ -0,0 +1,27 @@
+﻿namespace Lib.Cosmos.Tests.Adapters;
+
+[TestClass]
+public class CosmosContainerUpsertAdapterTests
+{
+    [TestMethod, TestCategory("unit")]
+    public void Should_Exist()
+    {
+        //arrange
+
+        //act
+        ICosmosContainerUpsertAdapter _ = new CosmosContainerUpsertAdapter();
+
+        //assert
+
+    }
+}
+
+public class CosmosContainerUpsertAdapter : ICosmosContainerUpsertAdapter
+{
+    public void Foo() => throw new System.NotImplementedException();
+}
+
+public interface ICosmosContainerUpsertAdapter
+{
+    void Foo();
+}
