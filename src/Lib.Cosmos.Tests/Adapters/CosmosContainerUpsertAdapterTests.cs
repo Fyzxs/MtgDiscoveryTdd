3cd6a009fd5c729c724988e5d374a187cd09270d
Quinn Gil <Fyzxs@users.noreply.github.com>
2025-04-03 21:48:44 -0700

    - f calls method

--- src/Lib.Cosmos.Tests/Adapters/CosmosContainerUpsertAdapterTests.cs
+++ src/Lib.Cosmos.Tests/Adapters/CosmosContainerUpsertAdapterTests.cs
@@ -1,4 +1,7 @@
-﻿using Lib.Cosmos.Adapters;
+﻿using System.Threading.Tasks;
+using Lib.Cosmos.Adapters;
+using Lib.Cosmos.Tests.Fakes;
+using Microsoft.Azure.Cosmos;
 
 namespace Lib.Cosmos.Tests.Adapters;
 
@@ -14,5 +17,24 @@ public sealed class CosmosContainerUpsertAdapterTests
         ICosmosContainerUpsertAdapter _ = new CosmosContainerUpsertAdapter();
 
         //assert
+    }
+
+    [TestMethod, TestCategory("unit")]
+    public async Task UpsertItemAsync_ShouldCallUpsertItemAsyncOnContainer()
+    {
+        //arrange
+        ItemResponseFake<CosmosItem> upsertItemAsyncResponse = new();
+        ContainerFake<CosmosItem> containerFake = new()
+        {
+            UpsertItemAsyncResponse = upsertItemAsyncResponse
+        };
+        CosmosItem cosmosItem = new();
+        CosmosContainerUpsertAdapter subject = new();
+
+        //act
+        ItemResponse<CosmosItem> actual = await subject.UpsertItemAsync<CosmosItem>(containerFake, cosmosItem).ConfigureAwait(false);
+
+        //assert
+        _ = actual.Should().BeSameAs(upsertItemAsyncResponse);
     }
 }
--- src/Lib.Cosmos/Adapters/CosmosContainerUpsertAdapter.cs
+++ src/Lib.Cosmos/Adapters/CosmosContainerUpsertAdapter.cs
@@ -1,6 +1,15 @@
-﻿namespace Lib.Cosmos.Adapters;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+using Microsoft.Azure.Cosmos;
+
+namespace Lib.Cosmos.Adapters;
 
 public sealed class CosmosContainerUpsertAdapter : ICosmosContainerUpsertAdapter
 {
     public void Foo() => throw new System.NotImplementedException();
+
+    public async Task<ItemResponse<T>> UpsertItemAsync<T>([NotNull] Container container, T item)
+    {
+        return await container.UpsertItemAsync(item).ConfigureAwait(false);
+    }
 }
