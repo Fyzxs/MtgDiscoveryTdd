b97bf57aa477c79cc4b96f4da5b1af52bf0cdeea
Quinn Gil <Fyzxs@users.noreply.github.com>
2025-04-03 22:31:12 -0700

    - f added logging

--- src/Lib.Cosmos.Tests/Adapters/CosmosContainerUpsertAdapterTests.cs
+++ src/Lib.Cosmos.Tests/Adapters/CosmosContainerUpsertAdapterTests.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using Lib.Cosmos.Adapters;
 using Lib.Cosmos.Apis;
 using Lib.Cosmos.Tests.Fakes;
+using Microsoft.Azure.Cosmos;
 using Microsoft.Extensions.Logging;
 
 namespace Lib.Cosmos.Tests.Adapters;
@@ -32,7 +34,12 @@ public sealed class CosmosContainerUpsertAdapterTests
         ItemResponseFake<CosmosItem> upsertItemAsyncResponse = new()
         {
             ResourceResult = cosmosItem,
-            StatusCodeResult = HttpStatusCode.MethodNotAllowed
+            StatusCodeResult = HttpStatusCode.MethodNotAllowed,
+            RequestChargeResult = 1234.56,
+            DiagnosticsResult = new CosmosDiagnosticsFake()
+            {
+                GetClientElapsedTimeResult = new TimeSpan(0, 12, 34, 56)
+            }
         };
         ContainerFake<CosmosItem> containerFake = new()
         {
@@ -47,14 +54,53 @@ public sealed class CosmosContainerUpsertAdapterTests
         //assert
         _ = actual.Value.Should().BeSameAs(cosmosItem);
     }
+
+    [TestMethod, TestCategory("unit")]
+    public async Task UpsertItemAsync_ShouldLogExpected()
+    {
+        //arrange
+        CosmosItem cosmosItem = new();
+        ItemResponseFake<CosmosItem> upsertItemAsyncResponse = new()
+        {
+            ResourceResult = cosmosItem,
+            StatusCodeResult = HttpStatusCode.MethodNotAllowed,
+            RequestChargeResult = 1234.56,
+            DiagnosticsResult = new CosmosDiagnosticsFake()
+            {
+                GetClientElapsedTimeResult = new TimeSpan(0, 12, 34, 56)
+            }
+        };
+        ContainerFake<CosmosItem> containerFake = new()
+        {
+            UpsertItemAsyncResponse = upsertItemAsyncResponse
+        };
+        LoggerFake loggerFake = new();
+        CosmosContainerUpsertAdapter subject = new(loggerFake);
+
+        //act
+        _ = await subject.UpsertItemAsync<CosmosItem>(containerFake, cosmosItem).ConfigureAwait(false);
+
+        //assert
+        _ = loggerFake.Logs.ToString().Should().Contain("UpsertItem cost: [RequestCharge=1234.56] [ElapsedTime=12:34:56]");
+    }
 }
 
 public sealed class LoggerFake : ILogger
 {
-    public StringBuilder Logs { get; set; }
+    public StringBuilder Logs { get; } = new StringBuilder();
 
     public IDisposable BeginScope<TState>(TState state) where TState : notnull => throw new NotImplementedException();
-    public bool IsEnabled(LogLevel logLevel) => throw new NotImplementedException();
+    public bool IsEnabled(LogLevel logLevel) => true;
 
     public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter) => Logs.AppendLine(formatter(state, exception));
+}
+
+public sealed class CosmosDiagnosticsFake : CosmosDiagnostics
+{
+    public TimeSpan GetClientElapsedTimeResult { get; set; }
+    public override TimeSpan GetClientElapsedTime() => GetClientElapsedTimeResult;
+
+    public override string ToString() => throw new NotImplementedException();
+
+    public override IReadOnlyList<(string regionName, Uri uri)> GetContactedRegions() => throw new NotImplementedException();
 }
--- src/Lib.Cosmos/Adapters/CosmosContainerUpsertAdapter.cs
+++ src/Lib.Cosmos/Adapters/CosmosContainerUpsertAdapter.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
 using Lib.Cosmos.Apis;
 using Lib.Cosmos.OpResponses;
@@ -9,14 +10,26 @@ namespace Lib.Cosmos.Adapters;
 
 public sealed class CosmosContainerUpsertAdapter : ICosmosContainerUpsertAdapter
 {
+    private readonly ILogger _logger;
+
     public CosmosContainerUpsertAdapter(ILogger logger)
     {
+        _logger = logger;
     }
 
     public async Task<OpResponse<T>> UpsertItemAsync<T>([NotNull] Container container, T item)
     {
         ItemResponse<T> itemResponse = await container.UpsertItemAsync(item).ConfigureAwait(false);
-
+        _logger.UpsertInformation(itemResponse.RequestCharge, itemResponse.Diagnostics.GetClientElapsedTime());
         return new ItemOpResponse<T>(itemResponse);
     }
+}
+
+public static partial class CosmosContainerUpsertAdapterLoggerExtensions
+{
+    [LoggerMessage(
+        Level = LogLevel.Information,
+        Message = "UpsertItem cost: [RequestCharge={requestCharge}] [ElapsedTime={elapsedTime}]")
+    ]
+    public static partial void UpsertInformation(this ILogger logger, double requestCharge, TimeSpan elapsedTime);
 }
