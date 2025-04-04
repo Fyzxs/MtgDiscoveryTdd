f1630fc0aa97e977114c789f32f807eb9531516c
Quinn Gil <Fyzxs@users.noreply.github.com>
2025-04-03 22:00:27 -0700

    . r raise function

--- src/Lib.Cosmos/Adapters/ICosmosContainerUpsertAdapter.cs
+++ src/Lib.Cosmos/Adapters/ICosmosContainerUpsertAdapter.cs
@@ -1,6 +1,12 @@
-﻿namespace Lib.Cosmos.Adapters;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+using Lib.Cosmos.Apis;
+using Microsoft.Azure.Cosmos;
+
+namespace Lib.Cosmos.Adapters;
 
 public interface ICosmosContainerUpsertAdapter
 {
     void Foo();
+    Task<OpResponse<T>> UpsertItemAsync<T>([NotNull] Container container, T item);
 }
