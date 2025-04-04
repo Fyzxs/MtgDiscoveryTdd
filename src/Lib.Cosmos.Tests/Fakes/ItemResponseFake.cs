21625dab9dc89f8dd2dc1b346730b6857a8ad0e7
Quinn Gil <Fyzxs@users.noreply.github.com>
2025-04-03 22:21:29 -0700

    . t fake

--- src/Lib.Cosmos.Tests/Fakes/ItemResponseFake.cs
+++ src/Lib.Cosmos.Tests/Fakes/ItemResponseFake.cs
@@ -7,7 +7,12 @@ public sealed class ItemResponseFake<T> : ItemResponse<T>
 {
     public T ResourceResult { get; init; }
     public HttpStatusCode StatusCodeResult { get; init; }
+    public double RequestChargeResult { get; init; }
+    public CosmosDiagnostics DiagnosticsResult { get; init; }
 
     public override T Resource => ResourceResult;
     public override HttpStatusCode StatusCode => StatusCodeResult;
+    public override double RequestCharge => RequestChargeResult;
+
+    public override CosmosDiagnostics Diagnostics => DiagnosticsResult;
 }
