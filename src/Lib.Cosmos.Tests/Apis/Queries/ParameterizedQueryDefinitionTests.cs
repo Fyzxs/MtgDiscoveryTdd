fb4c1fcc3a3401a30c9c78e2c1cfcd6e55b1c7e1
Fyzxs <Fyzxs@users.noreply.github.com>
2025-05-26 16:48:32 -0700

    . F adding ParameterizedQueryDefinition

---
+++ src/Lib.Cosmos.Tests/Apis/Queries/ParameterizedQueryDefinitionTests.cs
@@ -0,0 +1,72 @@
+﻿using Lib.Cosmos.Apis.Queries;
+using Lib.Cosmos.Primitives;
+using Microsoft.Azure.Cosmos;
+
+namespace Lib.Cosmos.Tests.Apis.Queries;
+
+[TestClass]
+public class ParameterizedQueryDefinitionTests
+{
+    [TestMethod]
+    public void ShouldExist()
+    {
+        //arrange
+        ParameterizedQueryDefinition _ = new TestParameterizedQueryDefinition(null);
+
+        //act
+
+        //assert
+    }
+
+    [TestMethod]
+    public void Ctor_ShouldTakeQueryDefinition()
+    {
+        //arrange
+        QueryString queryString = new("some string");
+        ParameterizedQueryDefinition subject = new TestParameterizedQueryDefinition(queryString);
+
+        //act
+
+        //assert
+    }
+
+    [TestMethod]
+    public void AddParameter_ShouldReturnClass()
+    {
+        //arrange
+        QueryString queryString = new("some string");
+        ParameterizedQueryDefinition subject = new TestParameterizedQueryDefinition(queryString);
+
+        //act
+        ParameterizedQueryDefinition actual = subject.WithParameter("@param1", "value1");
+
+        //assert
+        actual.Should().BeSameAs(subject);
+    }
+
+    [TestMethod]
+    public void AddParameter_ShouldAddParam()
+    {
+        //arrange
+        QueryString queryString = new("some string");
+        ParameterizedQueryDefinition subject = new TestParameterizedQueryDefinition(queryString);
+
+        //act
+        QueryDefinition actual = subject
+                                        .WithParameter("@param1", "value1")
+                                        .WithParameter("@param2", "value2").AsSystemType();
+
+        //assert
+        (string name1, object value1) valueTuple1 = actual.GetQueryParameters()[0];
+        (string name2, object value2) valueTuple2 = actual.GetQueryParameters()[1];
+        valueTuple1.name1.Should().Be("@param1");
+        valueTuple1.value1.Should().Be("value1");
+        valueTuple2.name2.Should().Be("@param2");
+        valueTuple2.value2.Should().Be("value2");
+    }
+
+    private sealed class TestParameterizedQueryDefinition : ParameterizedQueryDefinition
+    {
+        public TestParameterizedQueryDefinition(QueryString queryString) : base(queryString) { }
+    }
+}
---
+++ src/Lib.Cosmos/Apis/Queries/ParameterizedQueryDefinition.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using Lib.Cosmos.Primitives;
+using Microsoft.Azure.Cosmos;
+
+namespace Lib.Cosmos.Apis.Queries;
+
+public abstract class ParameterizedQueryDefinition : SimpleQueryDefinition
+{
+    private readonly SimpleQueryDefinition _queryDefinition;
+    private readonly Dictionary<string, string> _params;
+
+    protected ParameterizedQueryDefinition(SimpleQueryDefinition queryDefinition) : this(queryDefinition, []) { }
+
+    private ParameterizedQueryDefinition(SimpleQueryDefinition queryDefinition, Dictionary<string, string> @params)
+    {
+        _queryDefinition = queryDefinition;
+        _params = @params;
+    }
+
+    public ParameterizedQueryDefinition WithParameter(string key, string value)
+    {
+        _params.Add(key, value);
+
+        return this;
+    }
+
+    public override QueryDefinition AsSystemType()
+    {
+        QueryDefinition queryDefinition = _queryDefinition.AsSystemType();
+
+        foreach (KeyValuePair<string, string> param in _params)
+        {
+            queryDefinition.WithParameter(param.Key, param.Value);
+        }
+
+        return queryDefinition;
+    }
+}
