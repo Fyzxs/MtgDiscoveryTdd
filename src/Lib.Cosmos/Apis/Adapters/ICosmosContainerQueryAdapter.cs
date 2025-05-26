1f71ee2389f4f59e731a089e2f9d40598d7d1639
Fyzxs <Fyzxs@users.noreply.github.com>
2025-05-26 16:49:30 -0700

    . d add xml docs

--- src/Lib.Cosmos/Apis/Adapters/ICosmosContainerQueryAdapter.cs
+++ src/Lib.Cosmos/Apis/Adapters/ICosmosContainerQueryAdapter.cs
@@ -5,7 +5,18 @@ using Microsoft.Azure.Cosmos;
 
 namespace Lib.Cosmos.Apis.Adapters;
 
+/// <summary>
+/// Defines an adapter interface for querying items from a Cosmos DB container.
+/// </summary>
 public interface ICosmosContainerQueryAdapter
 {
+    /// <summary>
+    /// Executes a query against the specified Cosmos DB container and returns the results as an enumerable of type <typeparamref name="T"/>.
+    /// </summary>
+    /// <typeparam name="T">The type of the items to be returned.</typeparam>
+    /// <param name="container">The Cosmos DB container to query.</param>
+    /// <param name="queryDefinition">The query definition to execute.</param>
+    /// <param name="partitionKey">The partition key to use for the query.</param>
+    /// <param name="cancellationToken">A token to observe while waiting for the task to complete.</param>
+    /// <returns>A task that represents the asynchronous operation. The task result contains an enumerable of items of type <typeparamref name="T"/>.</returns>
     Task<IEnumerable<T>> QueryAsync<T>(Container container, QueryDefinition queryDefinition, PartitionKey partitionKey, CancellationToken cancellationToken = default);
-}
--- src/Lib.Cosmos/Apis/Adapters/ICosmosContainerUpsertAdapter.cs
+++ src/Lib.Cosmos/Apis/Adapters/ICosmosContainerUpsertAdapter.cs
@@ -4,7 +4,20 @@ using Microsoft.Azure.Cosmos;
 
 namespace Lib.Cosmos.Apis.Adapters;
 
+/// <summary>
+/// Defines an adapter interface for upserting items into a Cosmos DB container.
+/// </summary>
 public interface ICosmosContainerUpsertAdapter
 {
+    /// <summary>
+    /// Upserts (inserts or updates) an item in the specified Cosmos DB container.
+    /// </summary>
+    /// <typeparam name="T">The type of the item to upsert.</typeparam>
+    /// <param name="container">The Cosmos DB container where the item will be upserted.</param>
+    /// <param name="item">The item to upsert.</param>
+    /// <returns>
+    /// A task that represents the asynchronous operation. The task result contains an <see cref="OpResponse{T}"/>
+    /// representing the outcome of the upsert operation.
+    /// </returns>
     Task<OpResponse<T>> UpsertItemAsync<T>(Container container, T item);
 }
