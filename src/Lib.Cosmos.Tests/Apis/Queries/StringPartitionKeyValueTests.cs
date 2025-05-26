04a55992c909422025f9a6ce2adf179c6953c0cc
Fyzxs <Fyzxs@users.noreply.github.com>
2025-05-26 16:48:55 -0700

    - F Add stringPartitionKey

---
+++ src/Lib.Cosmos.Tests/Apis/Queries/StringPartitionKeyValueTests.cs
@@ -0,0 +1,30 @@
+﻿using Lib.Cosmos.Apis.Queries;
+using Microsoft.Azure.Cosmos;
+
+namespace Lib.Cosmos.Tests.Apis.Queries;
+
+[TestClass]
+public sealed class StringPartitionKeyValueTests
+{
+    [TestMethod]
+    public void ShouldExist()
+    {
+        //arrange
+        PartitionKeyValue subject = new StringPartitionKeyValue("value");
+        //act
+        //assert
+    }
+
+    [TestMethod]
+    public void AsSystemType_ShouldHaveProvidedValue()
+    {
+        //arrange
+        StringPartitionKeyValue subject = new("the_value");
+
+        //act
+        PartitionKey actual = subject.AsSystemType();
+
+        //assert
+        actual.Should().Be(new PartitionKey("the_value"));
+    }
+}
---
+++ src/Lib.Cosmos/Apis/Queries/StringPartitionKeyValue.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Azure.Cosmos;
+
+namespace Lib.Cosmos.Apis.Queries;
+
+public sealed class StringPartitionKeyValue : PartitionKeyValue
+{
+    private readonly string _source;
+
+    public StringPartitionKeyValue(string source) => _source = source;
+
+    public override PartitionKey AsSystemType() => new(_source);
+}
