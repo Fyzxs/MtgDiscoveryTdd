f42576e1daee0d2c034f0321e305e3b3d5a49a36
Fyzxs <Fyzxs@users.noreply.github.com>
2025-05-26 16:48:09 -0700

    - F Add Query String class

---
+++ src/Lib.Cosmos.Tests/Primitives/QueryStringTests.cs
@@ -0,0 +1,34 @@
+﻿using Lib.Cosmos.Primitives;
+using Lib.Universal.Primitives;
+using Microsoft.Azure.Cosmos;
+
+namespace Lib.Cosmos.Tests.Primitives;
+
+[TestClass]
+public sealed class QueryStringTests
+{
+    [TestMethod]
+    public void ShouldExist()
+    {
+        //arrange
+        ToSystemType<QueryDefinition> subject = new QueryString("My Query String");
+
+        //act
+
+        //assert
+    }
+
+    [TestMethod]
+    public void QueryDefinition_ShouldHaveProvided()
+    {
+        //arrange
+        const string QueryString = "Does Not Matter";
+        ToSystemType<QueryDefinition> subject = new QueryString(QueryString);
+
+        //act
+        QueryDefinition queryDefinition = subject.AsSystemType();
+
+        //assert
+        _ = queryDefinition.QueryText.Should().Be(QueryString);
+    }
+}
---
+++ src/Lib.Cosmos/Primitives/QueryString.cs
@@ -0,0 +1,13 @@
+﻿using Lib.Universal.Primitives;
+using Microsoft.Azure.Cosmos;
+
+namespace Lib.Cosmos.Primitives;
+
+public sealed class QueryString : ToSystemType<QueryDefinition>
+{
+    private readonly string _origin;
+
+    public QueryString(string origin) => _origin = origin;
+
+    public override QueryDefinition AsSystemType() => new(_origin);
+}
