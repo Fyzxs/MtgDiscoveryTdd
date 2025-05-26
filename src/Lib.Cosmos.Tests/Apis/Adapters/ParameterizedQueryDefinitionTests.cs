68ec7ac3681831cc8e4a8bdcb20f8844d9cd2e93
Fyzxs <Fyzxs@users.noreply.github.com>
2025-05-26 16:47:58 -0700

    . F adding base parameterized query definition

---
+++ src/Lib.Cosmos.Tests/Apis/Adapters/ParameterizedQueryDefinitionTests.cs
@@ -0,0 +1,29 @@
+﻿using Lib.Universal.Primitives;
+using Microsoft.Azure.Cosmos;
+
+namespace Lib.Cosmos.Tests.Apis.Adapters;
+
+[TestClass]
+public class ParameterizedQueryDefinitionTests
+{
+    [TestMethod]
+    public void ShouldExist()
+    {
+        //arrange
+        ParameterizedQueryDefinition subject = new TestParameterizedQueryDefinition();
+
+        //act
+
+        //assert
+    }
+
+    private sealed class TestParameterizedQueryDefinition : ParameterizedQueryDefinition
+    {
+        public override QueryDefinition AsSystemType() => throw new System.NotImplementedException();
+    }
+}
+
+public abstract class ParameterizedQueryDefinition : ToSystemType<QueryDefinition>
+{
+}
+
