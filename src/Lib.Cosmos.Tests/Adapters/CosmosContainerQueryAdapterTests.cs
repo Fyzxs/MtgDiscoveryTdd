96568047ea2baefcd6354dfd45626249cbfba3c8
Fyzxs <Fyzxs@users.noreply.github.com>
2025-05-26 16:47:45 -0700

    . R test to create class

--- src/Lib.Cosmos.Tests/Adapters/CosmosContainerQueryAdapterTests.cs
+++ src/Lib.Cosmos.Tests/Adapters/CosmosContainerQueryAdapterTests.cs
@@ -1,8 +1,32 @@
 ﻿using Lib.Cosmos.Tests.Fakes;
+using Microsoft.Extensions.Logging;
 
 namespace Lib.Cosmos.Tests.Adapters;
 
 [TestClass]
 public sealed class CosmosContainerQueryAdapterTests
+{
+    [TestMethod, TestCategory("unit")]
+    public void Should_Exist()
+    {
+        //arrange
+
+        //act
+        LoggerFake loggerFake = new();
+        ICosmosContainerQueryAdapter _ = new CosmosContainerQueryAdapter(loggerFake);
+
+        //assert
+    }
+}
+
+public sealed class CosmosContainerQueryAdapter : ICosmosContainerQueryAdapter
+{
+    public CosmosContainerQueryAdapter(ILogger logger)
+    {
+        throw new System.NotImplementedException();
+    }
+}
+
+public interface ICosmosContainerQueryAdapter
 {
 }
