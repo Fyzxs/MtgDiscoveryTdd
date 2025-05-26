af2e84a6607912e2685aefc3163fdf75b561994a
Fyzxs <Fyzxs@users.noreply.github.com>
2025-05-26 16:48:48 -0700

    - F add PartitionKey

---
+++ src/Lib.Cosmos.Tests/Apis/Queries/PartitionKeyValueTests.cs
@@ -0,0 +1,8 @@
+﻿using Lib.Cosmos.Apis.Queries;
+using Microsoft.Azure.Cosmos;
+using TestConvenience.Universal.Tests;
+
+namespace Lib.Cosmos.Tests.Apis.Queries;
+
+[TestClass]
+public sealed class PartitionKeyValueTests : BaseToSystemTypeTests<PartitionKey, PartitionKeyValue>;
