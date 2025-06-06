180d6637ad7a23d3ee4d9b17bf596c63edb80e03
Quinn Gil <Fyzxs@users.noreply.github.com>
2025-06-05 21:53:19 -0700

    test less to try out agent test writing

---
+++ src/Lib.Cosmos/Adapters/MonoStateCosmosClientAdapter.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading;
+using Lib.Cosmos.Apis.Adapters;
+using Lib.Cosmos.Apis.Ids;
+using Microsoft.Azure.Cosmos;
+
+namespace Lib.Cosmos.Adapters;
+
+public sealed class MonoStateCosmosClientAdapter : ICosmosClientAdapter
+{
+    private static ICosmosClientAdapter s_adapter;
+    private static readonly SemaphoreSlim s_semaphore = new(1, 1);
+
+    private readonly ICosmosClientAdapterFactory _factory;
+
+    public MonoStateCosmosClientAdapter(ICosmosClientAdapterFactory factory) => _factory = factory;
+
+    private ICosmosClientAdapter MonoState()
+    {
+        if (s_adapter is not null) return s_adapter;
+
+        s_semaphore.Wait();
+        try
+        {
+#pragma warning disable CA1508 // Avoid dead conditional code; it thinks it's always null
+            return s_adapter ??= _factory.Instance();
+#pragma warning restore CA1508
+        }
+        finally
+        {
+            _ = s_semaphore.Release();
+        }
+    }
+
+    public Container GetContainer(CosmosDatabaseName databaseName, CosmosCollectionName collectionName) => MonoState().GetContainer(databaseName, collectionName);
+}
