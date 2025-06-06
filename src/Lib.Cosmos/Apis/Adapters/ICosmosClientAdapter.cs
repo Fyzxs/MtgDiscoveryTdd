29bb5950ceb382a24ad5f0dc428767287fd298e9
Quinn Gil <Fyzxs@users.noreply.github.com>
2025-06-05 21:31:53 -0700

    adding support interfaces

---
+++ src/Lib.Cosmos/Apis/Adapters/ICosmosClientAdapter.cs
@@ -0,0 +1,18 @@
+﻿using Lib.Cosmos.Apis.Ids;
+using Microsoft.Azure.Cosmos;
+
+namespace Lib.Cosmos.Apis.Adapters;
+
+/// <summary>
+/// Adapter interface for interacting with a Cosmos DB client.
+/// </summary>
+public interface ICosmosClientAdapter
+{
+    /// <summary>
+    /// Gets a Cosmos DB container.
+    /// </summary>
+    /// <param name="databaseName">The name of the Cosmos DB database.</param>
+    /// <param name="collectionName">The name of the Cosmos DB collection (container).</param>
+    /// <returns>The <see cref="Container"/> instance.</returns>
+    Container GetContainer(CosmosDatabaseName databaseName, CosmosCollectionName collectionName);
+}
---
+++ src/Lib.Cosmos/Apis/Adapters/ICosmosClientAdapterFactory.cs
@@ -0,0 +1,13 @@
+﻿namespace Lib.Cosmos.Apis.Adapters;
+
+/// <summary>
+/// Factory interface for creating instances of <see cref="ICosmosClientAdapter"/>.
+/// </summary>
+public interface ICosmosClientAdapterFactory
+{
+    /// <summary>
+    /// Creates an instance of <see cref="ICosmosClientAdapter"/>.
+    /// </summary>
+    /// <returns>An instance of <see cref="ICosmosClientAdapter"/>.</returns>
+    ICosmosClientAdapter Instance();
+}
